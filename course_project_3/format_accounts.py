44ab8df1273f5a427fc04c05005fed14ea60f304
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-05-25 21:09:32 +0300

    add format_accounts.py

---
+++ course_project_3/format_accounts.py
@@ -0,0 +1,38 @@
+def format_from_account(input_from_account: str):
+    from_account_split = input_from_account.split(" ")
+
+    account_list = []
+    for i in from_account_split[1]:
+        account_list.append(i)
+
+    for i in range(6, 12):
+        account_list[i] = "*"
+
+    counter = 0
+    new_account = []
+
+    for num in account_list:
+        new_account.append(num)
+        counter += 1
+        if counter == 4:
+            new_account.append(' ')
+            counter = 0
+
+    del new_account[len(new_account) - 1]
+    from_account = ''.join(new_account)
+    from_account_split[1] = from_account
+
+    return " ".join(from_account_split)
+
+
+def format_to_account(input_to_account: str):
+    to_account_split = input_to_account.split(" ")
+    to_account_list = []
+    for i in to_account_split[1]:
+        to_account_list.append(i)
+
+    for num in range(0, 16):
+        to_account_list[num] = "*"
+    to_account_split[1] = "".join(to_account_list)[-6:]
+
+    return " ".join(to_account_split)
