ab73b1b1bb9a4cdf992bddaf71ac4ab7fa142864
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-05-26 14:07:34 +0300

    update executed_operations.py and test_executed_operations.py

--- course_project_3/executed_operations.py
+++ course_project_3/executed_operations.py
@@ -1,6 +1,9 @@
 def get_executed_operations(operation_list: list):
     executed_operations = []
     for operation in operation_list:
-        if operation['state'] == 'EXECUTED':
-            executed_operations.append(operation)
+        if 'state' in operation.keys():
+            if operation['state'] == 'EXECUTED':
+                executed_operations.append(operation)
+        else:
+            continue
     return executed_operations[0:5]
